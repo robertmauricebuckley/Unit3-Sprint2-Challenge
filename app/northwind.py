3d18806ddc6799bdb63536daae7fcfa841499319
Robert Buckley <robertworkbuckley@gmail.com>
2020-03-20 10:33:31 -0700

    comments added to answers

--- app/northwind.py
+++ app/northwind.py
@@ -10,6 +10,7 @@ print("CONNECTION:", conn)
 curs = conn.cursor()
 print("CURSOR", curs)
 
+# What are the ten most expensive items (per unit price) in the database?
 query = """
 SELECT 
     ProductName,
@@ -22,12 +23,30 @@ LIMIT 10;
 curs.execute(query)
 print(curs.fetchall())
 
+# Côte de Blaye	263.5
+# Thüringer Rostbratwurst	123.79
+# Mishi Kobe Niku	97
+# "Sir Rodney's Marmalade"	81
+# Carnarvon Tigers	62.5
+# Raclette Courdavault	55
+# Manjimup Dried Apples	53
+# Tarte au sucre	49.3
+# Ipoh Coffee	46
+# Rössle Sauerkraut	45.6
+
+
+
+#What is the average age of an employee at the time of their hiring?
 query2 = """
 SELECT AVG (HireDate), AVG (BirthDate)
 FROM Employee
 """
 curs.execute(query2)
 print(curs.fetchall())
+
+# 2025.0	1987.77777777778
+
+
 
 # Part 3
 # What are the ten most expensive items (per unit price) in the database and their suppliers?
@@ -43,6 +62,19 @@ LIMIT 10
 curs.execute(query3)
 print(curs.fetchall())
 
+# 18	Côte de Blaye	Aux joyeux ecclésiastiques	263.5
+# 12	Thüringer Rostbratwurst	Plutzer Lebensmittelgroßmärkte AG	123.79
+# 4	Mishi Kobe Niku	Tokyo Traders	97
+# 8	"Sir Rodney's Marmalade"	Specialty Biscuits, Ltd.	81
+# 7	Carnarvon Tigers	Pavlova, Ltd.	62.5
+# 28	Raclette Courdavault	Gai pâturage	55
+# 24	Manjimup Dried Apples	"G'day, Mate"	53
+# 29	Tarte au sucre	"Forêts d'érables"	49.3
+# 20	Ipoh Coffee	Leka Trading	46
+# 12	Rössle Sauerkraut	Plutzer Lebensmittelgroßmärkte AG	45.6
+
+
+
 # What is the largest category (by number of unique products in it)?
     #
 query4 = """
@@ -56,5 +88,7 @@ LIMIT 1
 """
 curs.execute(query4)
 print(curs.fetchall())
+
+# 13	Confections
 
 conn.commit()
